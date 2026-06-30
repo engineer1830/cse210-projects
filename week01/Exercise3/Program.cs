ff8ac93f95d4141a5a89214b0fad3310897e9239
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-06-29 23:10:39 -0700

    Exercise 3 week 01

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.Write("Enter the magic number (between 1 & 100): ");
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+        // string input = Console.ReadLine();
+        // int magic = int.Parse(input);
+        int guess = 0;
+        string answer;
+        int count = 1;
+
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (guess == magic)
+            {
+                Console.WriteLine("You guessed the magic number!");
+                Console.WriteLine($"You guessed the magic number in {count} guesses.");
+            }
+
+            else if (guess > magic)
+            {
+                count++;
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                count++;
+                Console.WriteLine("Higher");
+            }
+        }
+
+
+        
     }
 }
