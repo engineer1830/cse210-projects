0d5a468c4784263f3700751461c948b3fbcddbfb
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-07-04 08:58:31 -0700

    week2 Journal work

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator prompt = new PromptGenerator();
+        while (true)
+        {
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display journal entries");
+            Console.WriteLine("3. Save journal");
+            Console.WriteLine("4. Load journal");
+            Console.WriteLine("5. Quit");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string promptText = prompt.GetRandomPrompt();
+                Console.WriteLine(promptText);
+
+                string response = Console.ReadLine();
+
+                JournalEntry entry = new JournalEntry();
+                entry._date = DateTime.Now;
+                entry._prompt = promptText;
+                entry._content = response;
+
+                journal._entries.Add(entry);
+            }
+
+            if (choice == "2")
+            {
+                journal.Display();
+            }
+            if (choice == "3")
+            {
+                Console.WriteLine("Enter filename:");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+            if (choice == "4")
+            {
+                Console.WriteLine("Enter filename:");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+            if (choice == "5") break;
+        }
+
     }
 }
